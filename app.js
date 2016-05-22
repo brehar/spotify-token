262f3f25f9804e38e4dbaa949e2d1f5b310029d9
Brett Hartman <brett.hartman@mac.com>
2016-05-21 20:28:22 -0700

    allow cross-domain

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
 app.use(morgan('dev'));
+app.use(allowCrossDomain);
 
 var clientId = 'b52dc91988324fd697d1f2db77378d46',
     clientSecret = process.env.SPOTIFY_SECRET;
